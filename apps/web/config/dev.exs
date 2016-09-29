081154ed2a72f9779a8f89fcd2fc6fb79e939f03
Alexey Kuznetsov <me@kuznetsoff.io>
2016-09-29 23:00:07 +0300

    Add webpack for js

--- apps/web/config/dev.exs
+++ apps/web/config/dev.exs
@@ -11,7 +11,7 @@ config :web, AppTemplate.Web.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: []
+  watchers: [node: ["node_modules/webpack/bin/webpack.js", "--watch", "--color"]]
 
 
 # Watch static and templates for browser reloading.
